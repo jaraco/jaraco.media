9f9e531683bf4010027ec245496916bd3f491482
jaraco <none@none>
2010-03-12 02:18:52 +0000

    Added rudimentary support for iweb (requires /iweb at root)

--- jaraco/media/index.py
+++ jaraco/media/index.py
@@ -35,12 +35,39 @@ class Index(object):
 class Site:
 	@cherrypy.expose
 	def index(self):
+		res = self.for_iphone()
+		if res: return res
 		index = Index()
 		items = [
 			'<div><a href="{link}">{title}</a></div>'.format(**vars())
 			for title, link in zip(index, index.iter_links())
 			]
 		return '<html><body>%s</body></html>' % ''.join(items)
+
+	def for_iphone(self):
+		"""
+		iPhone user agent is
+		Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_1_2 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7D11 Safari/528.16
+		"""
+		if 'iPhone' not in cherrypy.request.headers['User-Agent']:
+			return
+		item_template = """<ul class="pageitem">
+	<li class="menu">
+		<a href="{link}">
+			<img alt="Description" src="/iweb/thumbs/basics.png" />
+			<span class="name">{title}</span>
+			<span class="arrow"></span>
+		</a>
+	</li>
+</ul>"""
+		index = Index()
+		items = '\n'.join(
+			item_template.format(**vars())
+			for title, link in zip(index, index.iter_links())
+			)
+		title = "Movies to Watch"
+		res = iweb_template.format(**vars())
+		return res
 
 	@classmethod
 	def setup_application(cls, root):
@@ -102,3 +129,34 @@ def serve():
 	cherrypy.engine.start()
 	cherrypy.engine.block()
 
+iweb_template = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="yes" name="apple-mobile-web-app-capable" />
+<meta content="text/html; charset=iso-8859-1" http-equiv="Content-Type" />
+<meta content="minimum-scale=1.0, width=device-width, maximum-scale=0.6667, user-scalable=no" name="viewport" />
+<link href="/iweb/css/style.css" rel="stylesheet" media="screen" type="text/css" />
+<script src="/iweb/javascript/functions.js" type="text/javascript"></script>
+<title>Title of your page</title>
+<meta content="keyword1,keyword2,keyword3" name="keywords" />
+<meta content="Description of your page" name="description" />
+</head>
+
+<body>
+<div id="topbar">
+	<div id="title">{title}</div>
+</div>
+<div id="content">
+
+{items}
+
+</div>
+<div id="footer">
+	<!-- Support iWebKit by sending us traffic; please keep this footer on your page, consider it a thank you for our work :-) -->
+	<a class="noeffect" href="http://iwebkit.net">Powered by iWebKit</a></div>
+
+</body>
+
+</html>
+"""
