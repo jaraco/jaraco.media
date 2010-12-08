5a3d29590681e7e33bf9da0d24c6a21c213a4085
Jason R. Coombs <jaraco@jaraco.com>
2010-12-07 23:00:14 -0500

    Added some code to facilitate multibrake

--- jaraco/media/handbrake.py
+++ jaraco/media/handbrake.py
@@ -1,20 +1,25 @@
+from __future__ import print_function
 import os
 import subprocess
 import optparse
+from jaraco.util import ui
+from path import path
 
 def get_handbrake_cmd():
-	input = os.environ['DVD']
+	input = os.environ.get('DVD', 'D:\\')
 	return ['handbrake', '-i', input, '-N', 'eng']
 
-def get_titles():
+def get_titles(root):
 	title_no = eval(raw_input('enter starting DVD title number> '))
+	if root.files():
+		print('last is', sorted(season_dir.files())[-1].basename())
 	episode = eval(raw_input('enter starting episode> '))
 	ext = '.mp4'
 	while True:
 		title = raw_input('enter title> ')
 		if not title: return
 		filename = '{episode:02} - {title}{ext}'.format(**vars())
-		yield ['-t', str(title_no), '-o', filename]
+		yield ['-t', str(title_no), '-o', root/filename]
 		title_no += 1
 		episode += 1
 
@@ -24,9 +29,25 @@ def quick_brake():
 	cmd = get_handbrake_cmd() + ['-L', '-o', dest]
 	subprocess.Popen(cmd).wait()
 
+def find_root():
+	root = path('~/Public/Videos/TV').expanduser()
+	choices = [showdir.basename() for showdir in root.dirs()]
+	show = ui.Menu(choices).get_choice('Choose show (blank for new)> ')
+	if not show:
+		show = raw_input('Show name> ')
+	show_dir = root/show
+	show_dir.makedirs_p()
+	choices = [seasondir.basename() for seasondir in show_dir.dirs()]
+	season = ui.Menu(choices).get_choice('Choose season (blank for new)> ')
+	if not season:
+		season = 'Season %d' % eval(raw_input('Season Number> '))
+	season_dir = show_dir/season
+	season_dir.makedirs_p()
+	return season_dir
+
 def multibrake():
-	titles = get_titles()
+	root = find_root()
 	options, args = optparse.OptionParser().parse_args()
-	for title in list(get_titles()):
+	for title in list(get_titles(root)):
 		args = get_handbrake_cmd() + args + title
 		subprocess.Popen(args).wait()
