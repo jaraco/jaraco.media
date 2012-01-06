112b7b66de94492a3b15738aa35a93ddc8ff23bb
Jason R. Coombs <jaraco@jaraco.com>
2012-01-05 21:30:16 -0500

    Made adjust-sub not execute if imported

--- scripts/adjust-sub.py
+++ scripts/adjust-sub.py
@@ -1,6 +1,11 @@
-#!python
+import datetime
+
 from jaraco.media.srt import SubEntry
 
-entries = SubEntry.get_entries('Sky Crawlers.srt.orig')
-entries = SubEntry.adjust_entries(entries, timedelta(seconds=-22.5))
-SubEntry.write_entries('Sky Crawlers.srt', entries)
+def main():
+	entries = SubEntry.get_entries('Sky Crawlers.srt.orig')
+	entries = SubEntry.adjust_entries(entries, datetime.timedelta(seconds=-22.5))
+	SubEntry.write_entries('Sky Crawlers.srt', entries)
+
+if __name__ == '__main__':
+	main()
