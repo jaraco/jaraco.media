8a9ef9d02216e854cf43ac449c4897f7b5da627c
Jason R. Coombs <jaraco@jaraco.com>
2012-02-15 08:38:00 -0700

    Enhanced adjust-sub script

--- scripts/adjust-sub.py
+++ scripts/adjust-sub.py
@@ -1,11 +1,22 @@
 import datetime
+import os
+import shutil
 
+import path
 from jaraco.media.srt import SubEntry
+from jaraco.util.string import local_format as lf
 
 def main():
-	entries = SubEntry.get_entries('Sky Crawlers.srt.orig')
-	entries = SubEntry.adjust_entries(entries, datetime.timedelta(seconds=-22.5))
-	SubEntry.write_entries('Sky Crawlers.srt', entries)
+	root = path.path(r'\\drake\videos\Movies')
+	name = 'House of Flying Daggers'
+	orig = root / lf('{name}.srt.orig')
+	upd = root / lf('{name}.srt')
+	if not os.path.exists(orig):
+		shutil.copy(upd, orig)
+	entries = SubEntry.get_entries(orig)
+	diff = datetime.timedelta(seconds=-4.5)
+	entries = SubEntry.adjust_entries(entries, diff)
+	SubEntry.write_entries(upd, entries)
 
 if __name__ == '__main__':
 	main()
