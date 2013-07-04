05b8656b2ab9cbdd8b72b5b1f74e26062f271120
Jason R. Coombs <jaraco@jaraco.com>
2013-07-04 09:59:13 -0400

    Use trove classifiers to specify supported Python versions

--- setup.py
+++ setup.py
@@ -34,7 +34,9 @@ setup_params = dict(
 	classifiers = [
 		"Development Status :: 4 - Beta",
 		"Intended Audience :: Developers",
-		"Programming Language :: Python",
+		"Programming Language :: Python :: 2.6",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
 	],
 	entry_points = {
 		'console_scripts': [
