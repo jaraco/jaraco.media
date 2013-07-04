92bde7524fa6a832cd691b388e8493b00c03dde9
Jason R. Coombs <jaraco@jaraco.com>
2013-07-03 22:04:11 -0400

    Disable large file support (doesn't play on xbox)

--- jaraco/media/handbrake.py
+++ jaraco/media/handbrake.py
@@ -30,7 +30,7 @@ def get_handbrake_cmd():
 		'--subtitle-forced', '--native-language', 'eng',
 		'--encoder', 'x264',
 		'--quality', str(quality),
-		'--large-file',
+		# '--large-file',
 	]
 
 def is_hidden(filepath):
