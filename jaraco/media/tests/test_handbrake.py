5a91023bbb16ba6e8b1ca95b3529d7f1ee8d1b53
Jason R. Coombs <jaraco@jaraco.com>
2021-08-03 22:27:14 -0400

    Prefer monkeypatch to mock

--- jaraco/media/tests/test_handbrake.py
+++ jaraco/media/tests/test_handbrake.py
@@ -1,15 +1,13 @@
+import subprocess
 from importlib_resources import files
 
-import mock
 import path
 
 from jaraco.media import handbrake, dvd
 
 
-@mock.patch(
-    'subprocess.check_output',
-    return_value=files(__package__).joinpath('multi title output.txt').read_bytes(),
-)
-def test_title_durations(check_output, monkeypatch):
+def test_title_durations(monkeypatch):
+    mocked = files(__package__).joinpath('multi title output.txt').read_bytes()
+    monkeypatch.setattr(subprocess, 'check_output', lambda *args, **kw: mocked)
     monkeypatch.setattr(dvd, 'get_source', lambda: path.Path('/'))
     handbrake.title_durations()
